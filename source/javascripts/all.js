7efd76b1287df4d52da724c8f3ec6a413ddfe381
Lucas Cheung <lucascheungls@gmail.com>
2019-08-16 21:59:23 +0100

    add scroll effects

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,60 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  bannerScroll();
+  aboutScroll();
+}
+
+
+
+function bannerScroll() {
+  const banner = document.querySelector(".banner-wrapper");
+  const lucas = document.querySelector("#banner-lucas");
+  const cheung = document.querySelector("#banner-cheung");
+  let distanceToTop = banner.getBoundingClientRect().top;
+  let itemTranslate = Math.min(0, distanceToTop / 4);
+  lucas.style.transform = `translateX(${itemTranslate}px)`;
+  cheung.style.transform = `translateX(${-itemTranslate}px)`;
+}
+
+function aboutScroll() {
+    const about = document.querySelector(".about-wrapper");
+    const tagline = document.querySelector(".about-container h1");
+    let distanceToTop = about.getBoundingClientRect().top;
+    let distanceToTopFromBottom = about.getBoundingClientRect().bottom;
+    let startPoint = window.innerHeight / 2;
+    let divRange = distanceToTopFromBottom - distanceToTop;
+    let aboutList = [
+      "Digital Marketing Specialist.",
+      "Web App Developer.",
+      "Pianist.",
+      "Snowboarder.",
+      "Rock Climber.",
+      "Hong Kong.",
+      "London.",
+      "ENTP.",
+      "26 Years Old.",
+      "Meatarian.",
+      "Full Time Badass."
+    ];
+    let aboutListLengthIndex = aboutList.length - 1;
+    let aboutIndex = Math.floor(
+      ((startPoint - distanceToTop) / divRange) * (aboutList.length)
+    );
+    if (Math.floor(aboutIndex < 0)) {
+      aboutIndex = 0;
+    } else if (aboutIndex > aboutListLengthIndex) {
+      aboutIndex = aboutListLengthIndex;
+    } else {
+      aboutIndex = aboutIndex;
+    }
+    console.log(startPoint);
+    tagline.innerHTML = aboutList[aboutIndex];
+
+}
